9ea44b41b281150c99c37f98663042f162801e71
at1468 <at1468@york.ac.uk>
2020-06-10 13:28:30 +0100

    Message response test

--- bot.py
+++ bot.py
@@ -12,4 +12,12 @@ client = discord.Client()
 async def on_ready():
     print(f'{client.user} has connected to Discord!')
 
+@client.event
+async def on_message(message):
+    if message.author == client.user:
+        return
+
+    if message.content == 'test':
+        await message.channel.send('Test!')
+
 client.run(TOKEN)
